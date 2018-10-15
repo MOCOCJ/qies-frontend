367a0ee46719380259edd876b5c885dcac304b3d
Clayton-Laptop <claytonwinsor@me.com>
2018-10-15 16:36:33 -0400

    ValidServiceList skeleton no spooks pls

---
+++ src/main/java/moco/qiesfrontend/session/ValidServicesList.java
@@ -0,0 +1,21 @@
+package moco.qiesfrontend.session;
+
+import moco.qiesfrontend.transaction.record.ServiceNumber;;
+import java.util.HashSet;
+import java.io.File;
+
+/**
+ * ValidServicesList
+ */
+public class ValidServicesList {
+
+    private HashSet<ServiceNumber> validService;
+
+    public ValidServicesList(File validServicesFile) {
+
+    }
+
+    public boolean isInList(ServiceNumber service) {
+
+    }
+}
