9b47dab465327a3114c6ec96d2d46d9200b59ecc
Michael Pirie <m.pirie12@gmail.com>
2018-10-15 15:29:44 -0400

    ServiceNumber class update
    Not complete. First push

--- src/main/java/moco/qiesfrontend/transaction/record/ServiceNumber.java
+++ src/main/java/moco/qiesfrontend/transaction/record/ServiceNumber.java
@@ -1,9 +1,20 @@
 package moco.qiesfrontend.transaction.record;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * ServiceNumber
  */
+@Getter
+@Setter
 public class ServiceNumber {
+    //Vars
+    private String number;
+    private final static String DEFULT = "00000";
 
+    public ServiceNumber(String number) {
+
+    }
     
 }
