bbafb5312f425c1717673e5f306c3aa74bb4226b
Unknown <omriharary@hotmail.com>
2018-10-15 16:34:21 -0400

    BEANS!

--- src/main/java/moco/qiesfrontend/session/SessionManager.java
+++ src/main/java/moco/qiesfrontend/session/SessionManager.java
@@ -1,9 +1,23 @@
 package moco.qiesfrontend.session;
 
+import java.io.File;
+
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * SessionManager
  */
+@Getter
+@Setter
 public class SessionManager {
 
     private Session session;
+    private ValidServicesList servicesList;
+    private File summaryFile;
+
+    public SessionManager(String validServicesFilePath, String summaryFilePath) {
+
+        this.session = new NoSession();
+    }
 }
