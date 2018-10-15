587971a889d3ddaf924c786f7fdb0f073750ecf9
Clayton Winsor <claytonwinsor@me.com>
2018-10-15 17:15:02 -0400

    ValidServicesList is done?

--- src/main/java/moco/qiesfrontend/session/ValidServicesList.java
+++ src/main/java/moco/qiesfrontend/session/ValidServicesList.java
@@ -2,6 +2,7 @@ package moco.qiesfrontend.session;
 
 import moco.qiesfrontend.transaction.record.ServiceNumber;;
 import java.util.HashSet;
+import java.io.BufferedReader;
 import java.io.File;
 
 /**
@@ -12,11 +13,19 @@ public class ValidServicesList {
     private HashSet<ServiceNumber> validService;
 
     public ValidServicesList(File validServicesFile) {
+        String line;
+        BufferedReader reader = new BufferedReader(new FileReader(validServicesFile));
 
+        while (line = reader.readLine() != null) {
+            validService.add(new ServiceNumber(Integer.parseInt(line)));
+        }
     }
 
     public boolean isInList(ServiceNumber service) {
-
+        if (validService.contains(service)) {
+            return true;
+        }
+        return false;
     }
 
 }
