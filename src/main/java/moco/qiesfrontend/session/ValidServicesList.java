9d07840c5c333676f0fac2fc0dfdc7da9931f1ed
Clayton Winsor <claytonwinsor@me.com>
2018-10-15 17:01:11 -0400

    idk

--- src/main/java/moco/qiesfrontend/session/ValidServicesList.java
+++ src/main/java/moco/qiesfrontend/session/ValidServicesList.java
@@ -1,18 +1,14 @@
 package moco.qiesfrontend.session;
 
-<<<<<<< HEAD
 import moco.qiesfrontend.transaction.record.ServiceNumber;;
 import java.util.HashSet;
 import java.io.File;
 
-=======
->>>>>>> 99ac6ef437a8d18e1825e8fde335805e78dea507
 /**
  * ValidServicesList
  */
 public class ValidServicesList {
 
-<<<<<<< HEAD
     private HashSet<ServiceNumber> validService;
 
     public ValidServicesList(File validServicesFile) {
@@ -22,7 +18,5 @@ public class ValidServicesList {
     public boolean isInList(ServiceNumber service) {
 
     }
-=======
-    
->>>>>>> 99ac6ef437a8d18e1825e8fde335805e78dea507
+
 }
