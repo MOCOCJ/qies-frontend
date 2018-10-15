d1d337bab53ec35eb235aeb446eab7e9aba9da24
Unknown <omriharary@hotmail.com>
2018-10-15 16:12:57 -0400

    Session stuff

--- src/main/java/moco/qiesfrontend/session/Session.java
+++ src/main/java/moco/qiesfrontend/session/Session.java
@@ -1,9 +1,11 @@
 package moco.qiesfrontend.session;
 
+import moco.qiesfrontend.transaction.TransactionQueue;
+
 /**
  * Session
  */
-public abstract class Session {
+public interface Session {
 
-    
+    TransactionQueue process();
 }
--- src/main/java/moco/qiesfrontend/transaction/TransactionQueue.java
+++ src/main/java/moco/qiesfrontend/transaction/TransactionQueue.java
@@ -1,9 +1,18 @@
 package moco.qiesfrontend.transaction;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import moco.qiesfrontend.transaction.record.TransactionRecord;
+
 /**
  * TransactionQueue
  */
 public class TransactionQueue {
 
-    
+    private Queue<TransactionRecord> records;
+
+    public TransactionQueue() {
+        records = new ArrayDeque<>();
+    }
 }
