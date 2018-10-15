43f7a8e423956fa526e174d0ba35547d0b057785
Michael Pirie <m.pirie12@gmail.com>
2018-10-15 17:43:50 -0400

    Error Check
    Fixed the errors within ValidServicesList

--- src/main/java/moco/qiesfrontend/session/ValidServicesList.java
+++ src/main/java/moco/qiesfrontend/session/ValidServicesList.java
@@ -1,9 +1,11 @@
 package moco.qiesfrontend.session;
 
-import moco.qiesfrontend.transaction.record.ServiceNumber;;
+import moco.qiesfrontend.transaction.record.ServiceNumber;
 import java.util.HashSet;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * ValidServicesList
@@ -13,12 +15,16 @@ public class ValidServicesList {
     private HashSet<ServiceNumber> validService = new HashSet<ServiceNumber>();
 
     public ValidServicesList(File validServicesFile) {
-        String line;
-        BufferedReader reader = new BufferedReader(new FileReader(validServicesFile));
+        try (BufferedReader br = new BufferedReader(new FileReader(validServicesFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                validService.add(new ServiceNumber(line));
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+		}
 
-        while (line = reader.readLine() != null) {
-            validService.add(new ServiceNumber(Integer.parseInt(line)));
-        }
     }
 
     public boolean isInList(ServiceNumber service) {
