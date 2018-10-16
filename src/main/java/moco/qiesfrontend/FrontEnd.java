662345e9f9c1a31d7d9520b1cf51349ff467a495
Unknown <omriharary@hotmail.com>
2018-10-16 14:17:57 -0400

    Prevent crash when run without args

--- src/main/java/moco/qiesfrontend/FrontEnd.java
+++ src/main/java/moco/qiesfrontend/FrontEnd.java
@@ -8,11 +8,14 @@ import lombok.extern.java.Log;
 public class FrontEnd {
 
     public static void main(String[] args) {
-        int exitCode = 0;
+        int exitCode = 1;
 
-        log.log(Level.INFO, "Something happened");
-        log.log(Level.INFO, args[0]);
-        log.log(Level.INFO, args[1]);
+        if (args.length == 2) {
+            log.log(Level.INFO, "Something happened");
+            log.log(Level.INFO, args[0]);
+            log.log(Level.INFO, args[1]);
+            exitCode = 0;
+        }
 
         System.exit(exitCode);
     }
