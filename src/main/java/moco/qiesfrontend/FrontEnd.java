e08cae17e38df6b326441c9977210424ae096977
Unknown <omriharary@hotmail.com>
2018-10-16 11:10:31 -0400

    Removed Spring for now
    It's not worth it for what I was going to use it for, we can do without for now. Can be easily re-added in the future.

--- src/main/java/moco/qiesfrontend/FrontEnd.java
+++ src/main/java/moco/qiesfrontend/FrontEnd.java
@@ -2,29 +2,13 @@ package moco.qiesfrontend;
 
 import java.util.logging.Level;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 import lombok.extern.java.Log;
 import moco.qiesfrontend.session.SessionManager;
 
-@SpringBootApplication
 @Log
-public class FrontEnd implements CommandLineRunner {
+public class FrontEnd {
 
     public static void main(String[] args) {
-        SpringApplication.run(FrontEnd.class, args);
+        log.log(Level.INFO, "Something happened");
     }
-
-    @Override
-    public void run(String... args) throws Exception {
-        log.log(Level.WARNING ,"SOMETHING HAPPENED");
-    }
-
-    /* @Bean
-    public SessionManager sessionManager(String validServicesFilePath, String summaryFilePath) {
-        return new SessionManager(validServicesFilePath, summaryFilePath);
-    } */
 }
