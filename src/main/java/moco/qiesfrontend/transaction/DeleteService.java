c9ef799ee3534300d33843d0b5b55f764f36ac02
Michael Pirie <m.pirie12@gmail.com>
2018-10-17 13:13:18 -0400

    DeleteService Base Implementation

--- src/main/java/moco/qiesfrontend/transaction/DeleteService.java
+++ src/main/java/moco/qiesfrontend/transaction/DeleteService.java
@@ -1,6 +1,8 @@
 package moco.qiesfrontend.transaction;
 
 import moco.qiesfrontend.session.Input;
+import moco.qiesfrontend.transaction.record.ServiceName;
+import moco.qiesfrontend.transaction.record.ServiceNumber;
 import moco.qiesfrontend.transaction.record.TransactionCode;
 import moco.qiesfrontend.transaction.record.TransactionRecord;
 
@@ -17,6 +19,29 @@ public class DeleteService extends Transaction {
 
     @Override
     public TransactionRecord makeTransaction(Input input) {
-        return record;
-    }
+        String serviceNumberIn;
+        String serviceNameIn;
+        ServiceNumber serviceNumber;
+        ServiceName serviceName;
+
+        serviceNumberIn = input.takeInput("Enter service number of the service you wish to delete.");
+        try {
+            serviceNumber = new ServiceNumber(serviceNumberIn);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid service number.");
+            return null;
+        }
+
+        serviceNameIn = input.takeInput("Enter service name of the service you wish to delete.");
+        try {
+            serviceName = new ServiceName(serviceNameIn);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid service name.");
+            return null;
+        }
+
+        record.setSourceNumber(serviceNumber);
+        record.setServiceName(serviceName);
+
+        return record;    }
 }
