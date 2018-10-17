3d173e0fdf7f0d62132e9ef26afd3f943883dae9
Michael Pirie <m.pirie12@gmail.com>
2018-10-17 13:05:10 -0400

    Cancel Ticket Base Implamentation

--- src/main/java/moco/qiesfrontend/transaction/CancelTicket.java
+++ src/main/java/moco/qiesfrontend/transaction/CancelTicket.java
@@ -1,6 +1,8 @@
 package moco.qiesfrontend.transaction;
 
 import moco.qiesfrontend.session.Input;
+import moco.qiesfrontend.transaction.record.NumberTickets;
+import moco.qiesfrontend.transaction.record.ServiceNumber;
 import moco.qiesfrontend.transaction.record.TransactionCode;
 import moco.qiesfrontend.transaction.record.TransactionRecord;
 
@@ -17,6 +19,30 @@ public class CancelTicket extends Transaction {
 
     @Override
     public TransactionRecord makeTransaction(Input input) {
+        String serviceNumberIn;
+        int numTicketsIn;
+        ServiceNumber serviceNumber;
+        NumberTickets numberTickets;
+
+        serviceNumberIn = input.takeInput("Enter service number of ticket you would like to cancel.");
+        try {
+            serviceNumber = new ServiceNumber(serviceNumberIn);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid service number.");
+            return null;
+        }
+
+        numTicketsIn = Integer.parseInt(input.takeInput("Enter number of tickets you wan."));
+        try {
+            numberTickets = new NumberTickets(numTicketsIn);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid number of tickets.");
+            return null;
+        }
+
+        record.setSourceNumber(serviceNumber);
+        record.setNumberTickets(numberTickets);
+
         return record;
     }
 }
